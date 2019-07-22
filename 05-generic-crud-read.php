5978388ecfab90df7a91de3e4fd74082743c86f0
moraismarcio <marcioc.morais@hotmail.com>
2019-07-22 14:58:04 -0300

    Imp. Generic Crud Read

---
+++ 05-generic-crud-read.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php 
+
+require('Conn.class.php');
+require('Read.class.php');
+
+$read = new Read;
+$read->ExeRead('logins', 'WHERE arquiteto = :arquiteto LIMIT :limit' , "arquiteto=1&limit=3");
+var_dump($read);
+echo "<hr>";
+
+$read->setPlaces('arquiteto=1&limit=3');
+
+var_dump($read);
+echo "<hr>";
+
+$read->fullRead("SELECT * FROM logins LIMIT :limit", "limit=2");
+
+var_dump($read);
+
+
+ ?>
+</body>
+</html>
