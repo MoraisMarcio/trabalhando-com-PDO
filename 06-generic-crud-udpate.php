85491ba9e5cbc0ad1a17d8057246a943c28387ad
moraismarcio <marcioc.morais@hotmail.com>
2019-07-23 15:48:52 -0300

    Implementando PDO Generic Crud Update

---
+++ 06-generic-crud-udpate.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php 
+
+require('Conn.class.php');
+require('Update.class.php');
+
+$Dados = [
+'senha' => 'a', 'arquiteto' => '1', 'lojista' => '0', 'cod_ativacao' => '0', 'status' => '0'
+];
+
+$update = new Update;
+$update->ExeUpdate('logins', $Dados, "WHERE email = :email", 'email=marciu_morais@hotmail.com');
+
+if ($update->getRowCount()>0) {
+	echo $update->getRowCount() ." registro(s) atualizado(s) com sussesso!<hr>";
+}else{
+	echo "Não foi possível atualizar os dados informados!<hr>";
+}
+
+ ?>
+</body>
+</html>
---
+++ Update.class.php
@@ -0,0 +1,80 @@
+<?php 
+/**
+*<b> Update.class:</b>
+*Classe responsável por atualizações genéricas no banco de dados.	
+*/
+class Update extends Conn {
+	private $Tabela;
+	private $Dados;
+	private $Termos;
+	private $Places;
+	private $Result;
+
+	/** @var PDOStatement */
+	private $Update;
+	/** @var PDO */
+	private $Conn;
+
+	public function ExeUpdate($Tabela, array $Dados, $Termos, $ParseString) 
+	{
+		$this->Tabela = (string) $Tabela;
+		$this->Dados = $Dados;
+		$this->Termos = (string) $Termos;
+
+		parse_str($ParseString, $this->Places);
+		$this->getSyntax();
+		$this->Execute();
+	}
+
+	public function getResult()
+	{
+		return $this->Result;
+	}
+
+	public function getRowCount(){
+		return $this->Update->rowCount();
+	}
+
+	public function setPlaces($ParseString){
+		parse_str($ParseString, $this->Places);
+		$this->Execute();
+	}
+
+	/**
+	*	*************************************************
+	*	**************** MÉTODOS PRIVADOS ***************
+	*	*************************************************
+	*/
+
+	private function Connect()
+	{
+		$this->Conn = parent::getConn();
+		$this->Update = $this->Conn->prepare($this->Update);
+	}
+
+	private function getSyntax()
+	{
+		foreach ($this->Dados as $key => $value) {
+			$Places[] = $key . ' = :' . $key;
+		}
+
+		$Places = implode(', ', $Places);
+		$this->Update = 'UPDATE '. $this->Tabela . ' SET ' . $Places .' '. $this->Termos;
+	}
+
+	private function Execute()
+	{
+		$this->Connect();
+		try {
+			$this->Update->execute(array_merge($this->Dados, $this->Places));
+			$this->Result = true;
+		} catch (PDOException $e) {
+			$this->Result = null;
+				//WSErro("<b>Erro ao cadastrar:</b> {$e->getMessage()}", e->getCode());
+			echo "ERRO AO ATUALIZAR!";
+		}
+	}
+
+}
+
+?>
